0961bbd7d866f25d1f107ca3cc84767e85fc484b
Ciel <stacey.cielia.lynn@gmail.com>
2016-01-27 12:06:18 -0600

    Created a new XAML application. It has a cool name, but that's okay because glow worm made it, and glow worm has that level clearance.

---
+++ Training/Proctologist/Main.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Proctologist {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Main : Window {
+        public Main() {
+            InitializeComponent();
+        }
+    }
+}
