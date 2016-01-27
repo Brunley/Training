1d111806e2316d22d5744c20e5807123cc3564b2
Ciel <stacey.cielia.lynn@gmail.com>
2016-01-27 12:33:36 -0600

    Added a character class.

---
+++ Training/Proctologist/Models/Character.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Highworm {
+    public class Character {
+        public Character() {
+            Statistics = new List<Statistic>();
+        }
+
+        public IList<Statistic> Statistics {
+            get;
+            set;
+        }
+    }
+}
