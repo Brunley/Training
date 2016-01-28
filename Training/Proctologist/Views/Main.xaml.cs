7c3908ba93ee6aefaf1f476dd718533f1272bfcc
Ciel <stacey.cielia.lynn@gmail.com>
2016-01-28 09:30:31 -0600

    established first example of using the controller pattern.

--- Training/Proctologist/Views/Main.xaml.cs
+++ Training/Proctologist/Views/Main.xaml.cs
@@ -1,5 +1,11 @@
 ﻿using System.Windows;
 
+using System;
+using System.Linq;
+
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Highworm {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -9,16 +15,23 @@ namespace Highworm {
             InitializeComponent();
 
             // initialize controllers
-            EncounterController = new Controllers.EncounterController();
-            SetupController = new Controllers.SetupController();
+            Encounter = new Controllers.EncounterController();
+            Setup = new Controllers.SetupController();
+
+            // begin creating characters
+            var characters = new List<Character>();
+
+            // use the controller to add new characters
+            characters.Add(Setup.Create("Amelia"));
+            characters.Add(Setup.Create("Daniel"));
         }
 
-        public Controllers.EncounterController EncounterController {
+        public Controllers.EncounterController Encounter {
             get;
             set;
         }
 
-        public Controllers.SetupController SetupController {
+        public Controllers.SetupController Setup {
             get;
             set;
         }
