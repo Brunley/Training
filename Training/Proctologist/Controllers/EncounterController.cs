92aba96fc2981bcd30a5a5d1e874c193cb69efec
Ciel <stacey.cielia.lynn@gmail.com>
2016-01-28 10:27:20 -0600

    Added Roll class. Removed Initiative method.

--- Training/Proctologist/Controllers/EncounterController.cs
+++ Training/Proctologist/Controllers/EncounterController.cs
@@ -8,17 +8,6 @@ using System.Collections.Generic;
 namespace Highworm.Controllers {
     public class EncounterController {
         /// <summary>
-        /// Roll initiative.
-        /// </summary>
-        /// <param name="modifier">
-        /// The modifier to apply to an initiative roll.
-        /// </param>
-        /// <returns></returns>
-        private decimal Initiative(decimal modifier) {
-            return new Irregular().Next(1, 20) + modifier;
-        }
-
-        /// <summary>
         /// Sort and order the participants based on their initiative.
         /// </summary>
         /// <typeparam name="T">
@@ -34,7 +23,7 @@ namespace Highworm.Controllers {
         public IList<T> Sort<T>(IList<T> participants, string modifier ) where T : ViewModels.Participant {
             // roll initiative for each participant
             foreach (var participant in participants)
-                participant.Order = Initiative(participant.Character.Statistics.Single(n => n.Name == modifier).Value);
+                participant.Order = new Roll(1, 20).Next().First() + (participant.Character.Statistics.Single(n => n.Name == modifier).Value);
             // return the adjusted collection, sorted
             return participants.OrderBy(n => n.Order).ToList();
         }
---
+++ Training/Proctologist/Infrastructure/Utilities/Roll.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace System {
+    /// <summary>
+    /// Represents a roll of the dice.
+    /// </summary>
+    public class Roll {
+        /// <summary>
+        /// Create a new dice roll.
+        /// </summary>
+        /// <param name="dice">
+        /// The number of dice to roll.
+        /// </param>
+        /// <param name="sides">
+        /// The number of sides on each die.
+        /// </param>
+        public Roll(int dice, int sides) {
+            this.Dice = dice;
+            this.Sides = sides;
+        }
+
+        /// <summary>
+        /// Perform a dice roll.
+        /// </summary>
+        /// <returns>
+        /// The result of rolling the dice as a collection.
+        /// </returns>
+        public IList<decimal> Next() {
+            // create a new collection to contain the
+            // results of our rolls
+            Results = new List<decimal>();
+
+            // for each die, perform a roll
+            for (int i = 0; i < Dice; i++)
+                Results.Add(new Irregular().Next(1, Sides));
+            // return the rolls
+            return Results;
+        }
+
+        /// <summary>
+        /// The dice roll results.
+        /// </summary>
+        public IList<decimal> Results {
+            get;
+            set;
+        }
+
+        private int Dice {
+            get;
+            set;
+        }
+
+        private int Sides {
+            get;
+            set;
+        }
+    }
+}
