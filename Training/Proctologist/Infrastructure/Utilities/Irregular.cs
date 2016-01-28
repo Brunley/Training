39b91bc0b95445425b35f45f67617b152cc06aad
Ciel <stacey.cielia.lynn@gmail.com>
2016-01-28 09:28:01 -0600

    Fixed an inheritence issue with Irregular.

--- Training/Proctologist/Infrastructure/Utilities/Irregular.cs
+++ Training/Proctologist/Infrastructure/Utilities/Irregular.cs
@@ -24,7 +24,7 @@ namespace System {
         }
 
         public override void GetNonZeroBytes(byte[] data) {
-            r.GetBytes(data);
+            r.GetNonZeroBytes(data);
         }
 
         ///<summary>
